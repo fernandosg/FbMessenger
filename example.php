f6518319c093259039941bc173baf93dc752442b
fernandosg <fsg_tmp@hotmail.com>
2017-07-27 20:20:07 -0500

    Implementing example with the new postback handler.

--- example.php
+++ example.php
@@ -4,7 +4,12 @@
   $page_id=7778878888;//YOUR PAGE_ID
   $msn=new Messenger($accesstoken,$page_id,json_decode(file_get_contents('php://input'), true));
   $msn->init();
-  $msn->sendMessage("This should be sending to the user after he/she interact with your bot");
-  $msn->sendMessageWithButton("This is the message that should be displayed in the box of button selection","Yes");
-  $msn->sendMessageWithMultipleButtons("This is the message that should be displayed in the box of multiple button selection",array(array("type"=>"postback","title"=>"I am agree","payload"=>"ACCEPTING"),array("type"=>"postback","title"=>"I am not agree","payload"=>"NOT_ACCEPTING")));
+  if($msn->isPostbackLike("ACCEPTING")){
+    $msn->sendMessage("Thanks for accepting");
+  }else if($msn->isPostbackLike("NOT_ACCEPTING")){
+    $msn->sendMessage("Maybe the next time =/");
+  }else{
+    $msn->sendMessageWithButton("This is the message that should be displayed in the box of button selection","Yes");
+    $msn->sendMessageWithMultipleButtons("This is the message that should be displayed in the box of multiple button selection",array(array("type"=>"postback","title"=>"I am agree","payload"=>"ACCEPTING"),array("type"=>"postback","title"=>"I am not agree","payload"=>"NOT_ACCEPTING")));
+  }
 ?>
